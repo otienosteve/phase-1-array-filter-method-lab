02cb3dadaec3116fd6a4393283ebe1be8540be0e
otienosteve <steveotieno701@gmail.com>
2022-04-23 10:38:19 +0300

    completed asssignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, match){
+    return drivers.filter(elem=>elem.toLowerCase()===match.toLowerCase())
+
+}
+function fuzzyMatch(drivers,match){
+ return   drivers.filter(driver=>driver.split('').splice(0,match.length).join('')===match)
+
+}
+let drivers=[]
+drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+function   matchName(drivers,match){
+    return drivers.filter(elem=>elem.name==match)
+
+}
